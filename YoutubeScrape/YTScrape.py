5bc8f9aaf3fc8494d61fe5fd25e776365301715f
Peter-Kang <pkang93@gmail.com>
2024-08-30 20:15:19 -0400

    Downloads the file, need to config a path

--- YoutubeScrape/YTScrape.py
+++ YoutubeScrape/YTScrape.py
@@ -1,16 +1,24 @@
-import pytube
+import yt_dlp
 import os 
 
 class YTScrape:
-    def __init__(self, links):
+    def __init__(self, links, dest):
         self.links = links
+        self.dest = dest
         
+    def download_audio(self, link):
+        with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
+            info_dict = video.extract_info(link, download = True)
+            video_title = info_dict['title']
+            print(video_title)
+            video.download(link)    
+            
+
     def Scrape(self):
         for link in self.links:
             try:
-                yt = pytube.YouTube( link )
-                # extract only audio 
-                video = yt.streams.filter(only_audio=True)
-                print(video)
+                #download youtube
+               self.download_audio(link)
+
             except Exception as error:
                 print(error)
